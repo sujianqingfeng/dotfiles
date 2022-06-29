fa1fff0b2372c511c3e8fa3e691edf2c43c074e1
hens <121116111@qq.com>
2022-06-29 20:24:23 +0800

    feat: lualine

--- nvim/lua/plugin-config/lualine.lua
+++ nvim/lua/plugin-config/lualine.lua
@@ -7,7 +7,7 @@ end
 
 lualine.setup({
   options = {
-    theme = "material-stealth",
+    theme = "auto",
     component_separators = { left = ">", right = "<" },
     -- https://github.com/ryanoasis/powerline-extra-symbols
     section_separators = { left = "", right = "" },
