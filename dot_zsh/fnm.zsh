5381d81cb10e91bfb3906fb5767fc82d7e390c78
hens <121116111@qq.com>
2024-01-16 09:53:35 +0800

    feat: add fnm $HOME path

--- dot_zsh/fnm.zsh
+++ dot_zsh/fnm.zsh
@@ -2,7 +2,7 @@
 if [[ "$(uname)" == "Darwin" ]]; then
   export PATH="/Users/hens/Library/Application Support/fnm:$PATH"
 elif [[ "$(expr substr $(uname -s) 1 5)" == "Linux" ]]; then
-  export PATH="/home/hens/.local/share/fnm:$PATH"
+  export PATH="$HOME/.local/share/fnm:$PATH"
   export PATH="/root/.local/share/fnm:$PATH"
 else
 fi
